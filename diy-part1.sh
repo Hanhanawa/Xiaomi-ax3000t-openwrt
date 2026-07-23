359d5aba37b051ff0fc0acdb16ce34648ab06da6
Hanhanawa <80897983+Hanhanawa@users.noreply.github.com>
2026-07-23 16:50:18 +0800

    Update diy-part1.sh
    Signed-off-by: Hanhanawa <80897983+Hanhanawa@users.noreply.github.com>

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,9 +9,7 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
-echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git' >>feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
